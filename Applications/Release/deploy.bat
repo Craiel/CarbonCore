73775bbfd6b2def23bdc77203176272e4d4bf9c9
Craiel <loft45@gmail.com>
2015-07-02 17:52:04 -0400

    Small update to Deploy scripts

--- Applications/Release/deploy.bat
+++ Applications/Release/deploy.bat
@@ -1,11 +1,12 @@
 @echo off
-echo
 
 SET STARTUP=%cd%
 SET APPLICATION=%1
 SET TARGET=%2
 
+echo:
 echo Deploying %APPLICATION% to %TARGET%
+echo:
 
 IF EXIST %TARGET% GOTO INSTALL
 mkdir %TARGET%
@@ -15,3 +16,7 @@ CD %~dp0\%APPLICATION%
 xcopy /Y *.dll %TARGET%
 xcopy /Y %APPLICATION%.exe* %TARGET%
 cd %STARTUP%
+
+echo:
+echo   - !DONE
+echo:
