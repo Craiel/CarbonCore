feea0a281179d2ebaf6bd1ee7245a3472914cbf9
Patrick Gustaevel <loft45@gmail.com>
2015-11-27 17:33:19 -0500

    Adding Enum tryparse extension

---
+++ UtilsUnity/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace CarbonCore.Utils.Unity
+{
+    using System;
+    using System.ComponentModel;
+
+    public static class EnumExtensions
+    {
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+
+        // Source: http://wiki.unity3d.com/index.php?title=EnumExtensions
+        public static bool TryParse<T>(string source, out T value)
+            where T : struct, IConvertible
+        {
+            value = default(T);
+            if (Enum.IsDefined(typeof(T), source))
+            {
+                TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+                value = (T)converter.ConvertFromString(source);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
