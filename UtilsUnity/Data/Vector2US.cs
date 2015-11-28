0fb5efdf47ff83e58e83e910208110ef87bcc32b
Craiel <loft45@gmail.com>
2015-11-28 05:45:51 -0500

    Added a small vector2 implementation for unsigned shorts

---
+++ UtilsUnity/Data/Vector2US.cs
@@ -0,0 +1,31 @@
+﻿namespace CarbonCore.Utils.Unity.Data
+{
+    public struct Vector2US
+    {
+        public static readonly Vector2US Zero = new Vector2US(0, 0);
+
+        // ------------------------------------------------------------------- 
+        // Public 
+        // ------------------------------------------------------------------- 
+        public Vector2US(ushort x, ushort y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public ushort X { get; private set; }
+
+        public ushort Y { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var typed = (Vector2US)obj;
+            return typed.X == this.X && typed.Y == this.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.X ^ this.Y;
+        }
+    }
+}
