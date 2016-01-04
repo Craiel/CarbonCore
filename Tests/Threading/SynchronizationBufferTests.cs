255ad82264f22162ef821206a9b3a28a36ff6435
Patrick Gustaevel <loft45@gmail.com>
2016-01-04 09:14:21 -0500

    Integrated Synchronization buffer - Removed mc sync launcher binaries and added to ignore

---
+++ Tests/Threading/SynchronizationBufferTests.cs
@@ -0,0 +1,73 @@
+﻿namespace CarbonCore.Tests.Threading
+{
+    using CarbonCore.Utils.Threading;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SynchronizationBufferTests
+    {
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        [Test]
+        public void TestReadWrite()
+        {
+            SynchronizationBuffer<DataHolder> buffer = new SynchronizationBuffer<DataHolder>();
+
+            var write = buffer.AquireWriteLock();
+            write.Data.Value = 0xBADF00D;
+            buffer.ReleaseLock(write);
+
+            var read = buffer.AquireReadLock();
+            Assert.AreEqual(0xBADF00D, read.Data.Value);
+            buffer.ReleaseLock(read);
+        }
+
+        [Test]
+        public void TestWriteRelevance()
+        {
+            SynchronizationBuffer<DataHolder> buffer = new SynchronizationBuffer<DataHolder>();
+
+            var write = buffer.AquireWriteLock();
+            write.Data.Value = 1;
+            buffer.ReleaseLock(write);
+
+            write = buffer.AquireWriteLock();
+            write.Data.Value = 2;
+            buffer.ReleaseLock(write);
+
+            var read = buffer.AquireReadLock();
+            Assert.AreEqual(2, read.Data.Value);
+            buffer.ReleaseLock(read);
+        }
+
+        [Test]
+        public void TestWriteLock()
+        {
+            SynchronizationBuffer<DataHolder> buffer = new SynchronizationBuffer<DataHolder>();
+
+            var write = buffer.AquireWriteLock();
+            write.Data.Value = 1;
+            buffer.ReleaseLock(write);
+
+            write = buffer.AquireWriteLock();
+            write.Data.Value = 2;
+
+            var read = buffer.AquireReadLock();
+            Assert.AreEqual(1, read.Data.Value);
+            buffer.ReleaseLock(read);
+
+            buffer.ReleaseLock(write);
+
+            read = buffer.AquireReadLock();
+            Assert.AreEqual(2, read.Data.Value);
+            buffer.ReleaseLock(read);
+        }
+
+        private class DataHolder
+        {
+            public int Value { get; set; }
+        }
+    }
+}
