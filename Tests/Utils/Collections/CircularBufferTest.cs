bf50e8a3014e23f16e7ec9dc136df78619dfcd73
Patrick Gustaevel <loft45@gmail.com>
2016-02-29 15:24:13 -0500

    Circular Buffer Integration

---
+++ Tests/Utils/Collections/CircularBufferTest.cs
@@ -0,0 +1,73 @@
+﻿namespace CarbonCore.Tests.Utils.Collections
+{
+    using CarbonCore.Utils.Collections;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class CicularBufferTests
+    {
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        [Test]
+        public void CicularBufferConstructorTest()
+        {
+            var buffer = new CircularBuffer<int>(1);
+            Assert.AreEqual(buffer.HaveValue(0), false);
+            Assert.Throws<ArgumentOutOfRangeException>(delegate() { buffer.Get(0); });
+        }
+
+        [Test]
+        public void CicularBufferAddTest()
+        {
+            var buffer = new CircularBuffer<int>(2);
+            buffer.Add(0);
+            buffer.Add(1);
+            Assert.AreEqual(buffer.Get(0), 0);
+            Assert.AreEqual(buffer.Get(1), 1);
+            buffer.Add(2);
+            Assert.Throws<ArgumentOutOfRangeException>(delegate() { buffer.Get(0); });
+            Assert.AreEqual(buffer.Get(1), 1);
+            Assert.AreEqual(buffer.Get(2), 2);
+        }
+
+        [Test]
+        public void CicularBufferSetTest()
+        {
+            var buffer = new CircularBuffer<int>(2);
+            buffer.Set(0, 0);
+            buffer.Set(1, 1);
+            Assert.AreEqual(buffer.Get(0), 0);
+            Assert.AreEqual(buffer.Get(1), 1);
+
+            buffer.Set(2, 2);
+            Assert.AreEqual(buffer.HaveValue(0), false);
+            Assert.AreEqual(buffer.Get(1), 1);
+            Assert.AreEqual(buffer.Get(2), 2);
+
+            buffer.Set(100, 100);
+            Assert.AreEqual(buffer.HaveValue(99), false);
+            Assert.AreEqual(buffer.Get(100), 100);
+
+            buffer.Add(101);
+            Assert.AreEqual(buffer.Get(100), 100);
+            Assert.AreEqual(buffer.Get(101), 101);
+        }
+
+        [Test]
+        public void CicularBufferSetTest2()
+        {
+            var buffer = new CircularBuffer<int>(3);
+            buffer.Set(0, 0);
+            buffer.Set(1, 1);
+            Assert.AreEqual(buffer.Get(0), 0);
+            Assert.AreEqual(buffer.Get(1), 1);
+
+            buffer.Set(3, 3);
+            Assert.AreEqual(buffer.Get(3), 3);
+            Assert.AreEqual(buffer.HaveValue(2), false);
+            Assert.AreEqual(buffer.Get(1), 1);
+        }
+    }
+}
---
+++ Utils/Collections/CircularBuffer.cs
@@ -0,0 +1,91 @@
+﻿namespace CarbonCore.Utils.Collections
+{
+    using System;
+
+    public class CircularBuffer<T>
+    {
+        private struct Data
+        {
+            public static Data Invalid = new Data { valid = false };
+
+            public Data(T value)
+            {
+                this.valid = true;
+                this.data = value;
+            }
+
+            public bool valid;
+            public T data;
+        }
+
+        Data[] buffer;
+        int length;
+        int nextFree;
+
+        public CircularBuffer(int length)
+        {
+            this.buffer = new Data[length];
+            this.length = length;
+            this.nextFree = 0;
+            for (int k = 0; k < length; k++)
+            {
+                this.buffer[k].valid = false;
+            }
+        }
+
+        public bool HaveValue(int index)
+        {
+            return IsIndexInRange(index) &&
+                   buffer[IndexToId(index)].valid;
+        }
+
+        public T Get(int index)
+        {
+            if (!HaveValue(index))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return buffer[IndexToId(index)].data;
+        }
+
+        public void Set(int index, T value)
+        {
+            if (IsIndexInRange(index))
+            {
+                buffer[IndexToId(index)] = new Data(value);
+            }
+            else if (index == nextFree)
+            {
+                Add(value);
+            }
+            else
+            {
+                int startIndex = nextFree;
+                int stopIndex = Math.Min(index, nextFree + length) - 1;
+                for (int k = startIndex; k <= stopIndex; k++)
+                {
+                    buffer[IndexToId(k)] = Data.Invalid;
+                }
+
+                buffer[IndexToId(index)] = new Data(value);
+                nextFree = index + 1;
+            }
+        }
+
+        public void Add(T value)
+        {
+            buffer[IndexToId(nextFree)] = new Data(value);
+            nextFree++;
+        }
+
+        private bool IsIndexInRange(int index)
+        {
+            return nextFree - length <= index && index < nextFree;
+        }
+
+        private int IndexToId(int index)
+        {
+            return index % length;
+        }
+    }
+}
