e11eb8e06679c4e2902e32884d47b44c2f4aa86f
Patrick Gustaevel <loft45@gmail.com>
2016-03-16 14:35:29 -0400

    - Added Nullable override for Float Extension with precision - Added SaveToJPG extension for Texture2D

--- Utils/MathUtils/FloatExtensions.cs
+++ Utils/MathUtils/FloatExtensions.cs
@@ -7,7 +7,7 @@
         // -------------------------------------------------------------------
         // Public
         // -------------------------------------------------------------------
-        public static bool NullableEquals(this float? a, float? b)
+        public static bool NullableEquals(this float? a, float? b, float precision)
         {
             if (a == null)
             {
@@ -19,7 +19,12 @@
                 return false;
             }
 
-            return Math.Abs(a.Value - b.Value) < float.Epsilon;
+            return Math.Abs(a.Value - b.Value) < precision;
+        }
+
+        public static bool NullableEquals(this float? a, float? b)
+        {
+            return a.NullableEquals(b, float.Epsilon);
         }
     }
 }
