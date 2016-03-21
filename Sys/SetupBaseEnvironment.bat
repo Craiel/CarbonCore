2b4b04b399673ab3f05dc08d6566da4ce465fc28
Patrick Gustaevel <loft45@gmail.com>
2016-03-21 10:29:02 -0400

    Adding support for VS2013

--- Sys/SetupBaseEnvironment.bat
+++ Sys/SetupBaseEnvironment.bat
@@ -6,13 +6,32 @@ ECHO Setting up CarbonCore Base Environment
 ECHO ---------------------------------
 ECHO.
 
+IF [%VSVER%] NEQ [] GOTO SetupVSSettings
+ECHO Setting Default VS Version to 2015
+ECHO.
+SET VSVER=2015
+
+:SetupVSSettings
+if [%VSVER%] == [2015] GOTO SetupVSSettings2015
+if [%VSVER%] == [2013] GOTO SetupVSSettings2013
+exit
+
+:SetupVSSettings2013
+SET VSVERSION=12.0
+GOTO SetupEnvironment
+
+:SetupVSSettings2015
+SET VSVERSION=14.0
+GOTO SetupEnvironment
+
+:SetupEnvironment
 SET CCSYSDIR=%~dp0
 SET CCROOTDIR=%CCSYSDIR%..\
 SET CCSOURCEDIR=%CCROOTDIR%Source\
 SET CCOUTDIR=%CCROOTDIR%Build\
 SET CURRENTDIR=%cd%\
-SET VSROOT=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7
-SET MSBUILDPATH="%ProgramFiles(x86)%\MSBuild\14.0\Bin"
+SET VSROOT=%ProgramFiles(x86)%\Microsoft Visual Studio %VSVERSION%\Common7
+SET MSBUILDPATH="%ProgramFiles(x86)%\MSBuild\%VSVERSION%\Bin"
 SET MSBUILD=%MSBUILDPATH%\msbuild.exe
 SET MSBUILDARGS=
 
