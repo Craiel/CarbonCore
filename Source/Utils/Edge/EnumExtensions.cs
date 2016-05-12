44be90306d704612833fe5988629c73c5ea8d61c
Patrick Gustaevel <loft45@gmail.com>
2016-05-12 18:53:14 -0400

    Adding Edge EnumExtension

---
+++ Source/Utils/Edge/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace CarbonCore.Utils.Edge
+{
+    using System;
+    using System.Globalization;
+
+    public static class EnumExtensions
+    {
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        public static bool TryParseInvariant<T>(string source, out T value)
+            where T : struct, IConvertible
+        {
+            if (Enum.TryParse(source, out value))
+            {
+                return true;
+            }
+
+            // Alternative method, we try to do a string comparison
+            foreach (T entry in Utils.EnumExtensions.GetValuesCached<T>())
+            {
+                if (entry.ToString(CultureInfo.InvariantCulture).Equals(source, StringComparison.OrdinalIgnoreCase))
+                {
+                    value = entry;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
