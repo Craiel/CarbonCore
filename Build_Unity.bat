a763bf58183d7b58bc8dad7daad5cfe0cc872942
Patrick Gustaevel <loft45@gmail.com>
2017-02-16 19:42:50 -0500

    cb update for unity

---
+++ Build_Unity.bat
@@ -0,0 +1,21 @@
+@echo off
+
+call Sys\SetupEnvironment.bat
+
+cls
+%MSBUILD% %MSBUILDARGS% "%SOLUTIONFILE%" /p:configuration=unity
+
+SET BUILD_STATUS=%ERRORLEVEL%
+
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+
+:fail 
+ECHO Failed building CC - Debug
+pause 
+exit /b 1 
+
+:end
+ECHO Done building CC - Debug
+pause
+exit /b 0 
