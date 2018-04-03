65ee40842981881ed4b45dcbf53c31a7969deeab
craiel <craieldev@gmail.com>
2018-04-03 17:11:28 -0400

    Update to support Unity 2017 structure

--- Sys/SetupEnvironment.bat
+++ Sys/SetupEnvironment.bat
@@ -2,9 +2,5 @@
 
 call Sys\SetupBaseEnvironment.bat
 
-SET INC_UNITYLIB=%CCROOTDIR%\External\Unity\5.5.0f3
-
 SET SOLUTIONFILE=%CCSOURCEDIR%\CarbonCore.sln
 SET SOLUTIONFILEUNITY=%CCSOURCEDIR%\CarbonCoreUnity.sln
-
-ECHO Unity Lib path set to %INC_UNITYLIB%
